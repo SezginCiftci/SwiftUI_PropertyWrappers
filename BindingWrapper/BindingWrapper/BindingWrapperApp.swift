18a7a045605cb2b33cde9d3bec905da51f2bd9f6
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2023-05-20 22:14:06 +0300

    Environment Wrapper Folder Added.

---
+++ BindingWrapper/BindingWrapper/BindingWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingWrapperApp.swift
+//  BindingWrapper
+//
+//  Created by Sezgin Çiftci on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BindingWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
