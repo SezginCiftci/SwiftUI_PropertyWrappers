15d6a8149d69bb20136bffe374209d43a6d9c48e
Sezgin Çiftçi <88513574+SezginCiftci@users.noreply.github.com>
2023-05-21 19:35:07 +0300

    Textfields and Info texts configured.

--- BindingWrapper/BindingWrapper/ContentView.swift
+++ BindingWrapper/BindingWrapper/ContentView.swift
@@ -15,29 +15,24 @@ struct ContentView: View {
     //MARK: Body
     var body: some View {
         VStack {
-            VStack {
-                SCTextField(username: $username)
-                    .padding(.vertical)
-                SCTextField2(password: $password)
-            }.padding()
+            Image("pug")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 150, height: 150)
+                .cornerRadius(75)
             
-            VStack {
-                HStack {
-                    Image(systemName: "xmark.circle.fill")
-                        .foregroundColor(Color.red)
-                    Text("Invalid username")
-                        .padding(.vertical)
-                        .foregroundColor(Color.red)
-                } //HStack
-                
-                HStack {
-                    Image(systemName: "checkmark.circle.fill")
-                        .foregroundColor(Color.red)
-                    Text("Valid password")
-                        .padding(.vertical)
-                        .foregroundColor(Color.red)
-                } //HStack
-            } //GroupBox
+            Text("WELCOME")
+                .padding(.bottom, 40)
+                .font(.system(.title, design: .serif))
+                .fontWeight(.bold)
+                .foregroundColor(Color.secondary)
+            
+            SCTextField(textFieldType: .username, text: $username)
+            SCInfoText(infoTextType: username.isValid ? .validUsername : .invalidUsername)
+            
+            SCTextField(textFieldType: .password, text: $password)
+            SCInfoText(infoTextType: password.isValid ? .validPassword : .invalidPassword)
+            
         } //VStack
     }
 }
@@ -50,43 +45,118 @@ struct ContentView_Previews: PreviewProvider {
 
 struct SCTextField: View {
     //MARK: Property
-    @Binding var username: String
+    var textFieldType: SCTextFieldType
+    @Binding var text: String
     
     //MARK: Body
     var body: some View {
         VStack {
             HStack {
-                Image(systemName: "person")
+                Image(systemName: textFieldType.textFieldImageName)
                     .foregroundColor(.secondary)
-                TextField("Username",
-                          text: $username)
+                switch textFieldType {
+                case .username:
+                    TextField(textFieldType.textFieldPlaceholder,
+                              text: $text)
+                case .password:
+                    SecureField(textFieldType.textFieldPlaceholder,
+                              text: $text)
+                }
+                
             } //HStack
             .padding()
             .background(Capsule().fill(Color.white))
             .frame(width: UIScreen.main.bounds.width - 100,
                    height: 50)
             .shadow(radius: 8)
-            
         } //VStack
     }
 }
 
-struct SCTextField2: View {
+struct SCInfoText: View {
     //MARK: Property
-    @Binding var password: String
+    var infoTextType: SCInfoTextType
     
     //MARK: Body
     var body: some View {
         HStack {
-            Image(systemName: "lock")
-                .foregroundColor(.secondary)
-            TextField("Password",
-                      text: $password)
+            Image(systemName: infoTextType.infoTextImage)
+                .foregroundColor(infoTextType.infoTextColor)
+            Text(infoTextType.infoText)
+                .foregroundColor(infoTextType.infoTextColor.opacity(0.8))
+        } //HStack
+        .padding(.top, 6)
+        .padding(.bottom)
+        .padding(.leading, 8)
+        .frame(width: UIScreen.main.bounds.width - 100, alignment: .leading)
+    }
+}
+
+enum SCTextFieldType {
+    case username
+    case password
+    
+    var textFieldPlaceholder: String {
+        switch self {
+        case .username:
+            return "Username"
+        case .password:
+            return "Password"
         }
-        .padding()
-        .background(Capsule().fill(Color.white))
-        .frame(width: UIScreen.main.bounds.width - 100,
-               height: 50)
-        .shadow(radius: 8)
+    }
+    
+    var textFieldImageName: String {
+        switch self {
+        case .username:
+            return "person"
+        case .password:
+            return "lock"
+        }
+    }
+}
+
+enum SCInfoTextType {
+    case invalidUsername
+    case validUsername
+    case invalidPassword
+    case validPassword
+    
+    var infoText: String {
+        switch self {
+        case .invalidUsername:
+            return "Invalid Username"
+        case .validUsername:
+            return "Valid Username"
+        case .invalidPassword:
+            return "Invalid Password"
+        case .validPassword:
+            return "Valid Password"
+        }
+    }
+    
+    var infoTextImage: String {
+        switch self {
+        case .invalidUsername, .invalidPassword:
+            return "xmark.circle.fill"
+        case .validUsername, .validPassword:
+            return "checkmark.circle.fill"
+        }
+    }
+    
+    var infoTextColor: Color {
+        switch self {
+        case .invalidUsername, .invalidPassword:
+            return .red
+        case .validUsername, .validPassword:
+            return .green
+        }
+    }
+}
+
+extension String {
+    var isValid: Bool {
+        let regex = ".*[^A-Za-z0-9].*"
+        let testString = NSPredicate(format:"SELF MATCHES %@", regex)
+        return testString.evaluate(with: self) && self.count >= 8 && !self.contains(" ")
     }
 }
